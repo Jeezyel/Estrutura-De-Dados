2efd186d74f8fd9946db01f55b1565eb848ada2d
Jeezyel P Costa <jeezyelpereira@gmail.com>
2023-04-26 09:47:55 -0300

    ajuste que o professor pedio

---
+++ Tree/Node.java
@@ -0,0 +1,26 @@
+public class Node {
+
+    private String info;
+    private Node liftNode = null;
+    private Node RightNode = null;
+    
+    public String getInfo() {
+        return info;
+    }
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    public Node getLiftNode() {
+        return liftNode;
+    }
+    public void setLiftNode(Node liftNode) {
+        this.liftNode = liftNode;
+    }
+    public Node getRightNode() {
+        return RightNode;
+    }
+    public void setRightNode(Node rightNode) {
+        RightNode = rightNode;
+    }
+
+}
